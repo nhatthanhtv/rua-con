49fe45d80b9d03517413ee634c1caf0275653214
Nhat Thanh <tangnhatthanh@gmail.com>
2022-02-24 10:25:10 +0700

    bài tập v1

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const btnSeach = document.querySelector('#search-btn')
 const input = document.querySelector('#search')
 
 const content = document.querySelector('.text__content')
+const data = document.querySelector('.data')
+const warning = document.querySelector('.warning')
+const keywordNumber = document.querySelector('.keyword__number')
 
+input.oninput = (e) => {
+    keywordNumber.innerText = ''
+    warning.innerText = ''
 
+}
 
 btnSeach.onclick = () =>{
     let value = input.value.trim()
@@ -12,12 +19,14 @@ btnSeach.onclick = () =>{
     const lengthValue = value.length
     const keyStart = content.innerText.indexOf(value)
 
-    if(keyStart === - 1)
-        return
-        
-    const content1 = content.innerText.slice(0,keyStart)
-    const content2 = content.innerText.slice(keyStart + lengthValue)
-    
-    const newContent = `${content1} <span>${content.innerText.slice(keyStart, keyStart + value.length)}</span>${content2}`
-    content.innerHTML = newContent
+    if(keyStart === - 1){
+        warning.innerText = 'Không tìm thấy nội dung'
+    }else{
+        const content1 = content.innerText.slice(0,keyStart)
+        const content2 = content.innerText.slice(keyStart + lengthValue)
+        keywordNumber.innerText = `Số lượng từ khóa: ${lengthValue}`
+        const newContent = `<p> ${content1} <span>${content.innerText.slice(keyStart, keyStart + value.length)}</span>${content2} </p>`
+        data.innerHTML = newContent
+    }
+
 }
